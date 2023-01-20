6c7ff664d54f290261628bcd536c8979a98e3b35
yosserdavanzo <yosserdavanzo0615@gmail.com>
2023-01-20 11:10:41 -0800

    rewrite scraper to class

--- src/dataCollection/scraper.py
+++ src/dataCollection/scraper.py
@@ -5,42 +5,72 @@ import os
 
 import json
 import csv
+import uuid
+
 from src.utils.serialProvider import gestureSerial
-from fileConstants import KEYS
+from fileConstants import *
 
-HEARTBEAT_FREQ = 10
-DURATION=500
-BACKOFF = 20/1000 # 20 ms as seconds
-FILE_NAME = 'temp.csv'
 
-print("Flushing Serial...")
-gestureSerial.flush()
-time.sleep(1)
-print("...Flushed!")
+class Scraper:
+    def __init__(self, folderPath:str,fileNamePrefix:str, dataCount:int = DATA_LENGTH):
+        self.HEARTBEAT_FREQ = 10
+        self.BACKOFF = 20/1000 # 20 ms as seconds
+        self.filePrefix = fileNamePrefix
+        self.dataCount = dataCount
 
-if os.path.isfile(FILE_NAME):
-    os.remove(FILE_NAME)
-    print("Deleted old temp file")
+        if os.path.isdir(folderPath):
+            raise Exception("Scrapper Path is not a folder")
+        self.folderPath = folderPath
 
-print("Hold Still!")
-for i in range(5):
-    try:
-        data = gestureSerial.readline().strip().split(b",")
-    except:
-        print("minor error found, attempting again....")
-    time.sleep(.1)
-input("Press [ENTER] to continue")
+        print("Flushing Serial...")
+        gestureSerial.flush()
+        time.sleep(1)
+        print("...Flushed!")
 
-with open(FILE_NAME, 'x', newline='') as f:
-    writ = csv.writer(f, delimiter=",")
-    writ.writerow(KEYS)
-    for i in range(DURATION):
-        if i%HEARTBEAT_FREQ == 0 : print(i)
-        try:
-            data = gestureSerial.readline().strip().split(b",")
-            data_spl = [float(x) for x in data]
-            if len(data_spl)>5:
-                writ.writerow([time.time(), *data_spl])
-        except:
-            print("failed loop")
-            time.sleep(BACKOFF)
+        self.calibrate()
+
+    def calibrate(self):
+        print("Hold Still!")
+        for i in range(5):
+            try:
+                # throw away data
+                _ = gestureSerial.readline().strip().split(b",")
+            except:
+                print("minor error found, attempting again....")
+            time.sleep(.1)
+        input("Press [ENTER] to continue")
+
+    def createFileName(self):
+        name = self.filePrefix + str(uuid.uuid4())
+        return os.path.join( self.folderPath, name)
+
+    def writeSingleFile(self) -> str:
+        fileName = self.createFileName()
+        with open(fileName, 'x', newline='') as f:
+            writ = csv.writer(f, delimiter=",")
+            writ.writerow(KEYS)
+            for i in range(self.dataCount):
+                if i%self.HEARTBEAT_FREQ == 0 : print(i)
+                try:
+                    data = gestureSerial.readline().strip().split(b",")
+                    data_spl = [float(x) for x in data]
+                    if len(data_spl) == KEY_COUNT:
+                        writ.writerow([time.time(), *data_spl])
+                except:
+                    print("failed loop")
+                    time.sleep(self.BACKOFF)
+        return fileName
+        
+    def writeMultiFile(self, totalFileCount:int):
+        rejectStr = '3'
+
+        count = 0
+        while count < totalFileCount:
+            print(f"\nFiles So Far: {count}\n")
+            file = self.writeSingleFile()
+            feedback = input(f"Keep the file?\n[{rejectStr}] for No, anything for yes")
+
+            if feedback.find(rejectStr) != -1:
+                os.remove(file)
+            else:
+                count += 1
